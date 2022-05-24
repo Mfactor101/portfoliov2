3595fc49941925443e916aa1e283418aa152f233
Abdulmuheez Owosho <muheez842@gmail.com>
2022-05-24 20:13:43 +0100

    extra files

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,25 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {       
+          "primary": "#53B7FF",        
+          "secondary": "#CF8ACF",      
+          "accent": "#C4C4C4",     
+          "neutral": "#575757",     
+          "base-100": "#2d2d2d",      
+          "info": "#3ABFF8",      
+          "success": "#36D399",      
+          "warning": "#FBBD23",      
+          "error": "#F87272",
+        },
+      },
+    ],
+  },
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 }
