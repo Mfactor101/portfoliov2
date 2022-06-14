e1dcf3a6287a57ba765ae31b7fceead02555cbb3
Abdulmuheez Owosho <muheez842@gmail.com>
2022-06-14 19:06:12 +0100

    Added the links page

---
+++ src/pages/Links.jsx
@@ -0,0 +1,31 @@
+import heroImage from '../img/hero-image-min.jpg';
+import { Link } from 'react-router-dom'
+
+function links() {
+  return (
+    <div className="flex h-screen justify-center">
+        <div className="container my-auto px-4 text-center">
+            <div className="py-6">
+                <img src={heroImage} alt="Muheez" className='block mx-auto w-1/5 rounded-full' />
+            </div>
+            <div className="">
+                <h1 className='text-3xl'>Muheez | Web Developer</h1>
+                <p>All my links 👇🏾</p>
+            </div>
+            <div className="p-6">
+                <div className="link-block bg-info p-4 mb-3 cursor-pointer">
+                    <Link to="/">My portfolio</Link>
+                </div>
+                <div className="link-block bg-info p-4 mb-3 cursor-pointer">
+                    <a href='https://www.instagram.com/muheezcodes/' rel="noreferrer" target="_blank">Instagram</a>
+                </div>
+                <div className="link-block bg-info p-4 mb-3 cursor-pointer">
+                    <a href='https://muheezdesign.gumroad.com/' rel="noreferrer" target="_blank">Gumroad</a>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default links
